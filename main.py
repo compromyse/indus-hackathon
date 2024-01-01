77a3ef6450434b287bc9d499f364294cefc6c3a9
Raghuram Subramani <raghus2247@gmail.com>
2024-01-01 15:47:19 +0530

    login

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from flask import Flask
+from flask import Flask, make_response, request
 from pysondb import db
 
 app = Flask(__name__)
 
 database = db.getDb('db.json')
+
+@app.route('/signup')
+def signup():
+    user = str(request.args.get('user'))
+    passw = str(request.args.get('passw'))
+
+    database.add({
+        'user': user,
+        'passw': passw
+    })
+
+    return 'OK'
  
-@app.route('/')
-def hello_name():
-   return 'Hello'
- 
+@app.route('/login')
+def login():
+    user = str(request.args.get('user'))
+    passw = str(request.args.get('passw'))
+
+    resp = make_response()
+
+    resp.set_cookie(
+        'id',
+        str(database.getByQuery({
+            'user': user,
+            'passw': passw
+        })[0]['id'])
+    )
+
+    return resp
+
+@app.route('/dashboard')
+def dashboard():
+    idx = request.cookies.get('id')
+    user = database.getById(int(idx))
+
+    return user['user']
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
