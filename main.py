01afad3a53a68cd34b6b09ee24a6d2e9a578575b
Raghuram Subramani <raghus2247@gmail.com>
2024-01-02 10:19:57 +0530

    dashboard

--- main.py
+++ main.py
@@ -1,49 +1,45 @@
-from flask import Flask, make_response, request, render_template
+from flask import Flask, make_response, redirect, request, render_template, url_for
 from pysondb import db
 
 app = Flask(__name__)
 
 database = db.getDb('db.json')
 
-@app.route('/signup')
-def signup():
-    user = str(request.args.get('user'))
-    passw = str(request.args.get('passw'))
+@app.route('/api_login', methods=['GET'])
+def login():
+    login = str(request.args.get('login'))
+    password = str(request.args.get('password'))
 
-    database.add({
-        'user': user,
-        'passw': passw
+    user = database.getByQuery({
+        'login': login,
+        'password': password
     })
 
-    return 'OK'
- 
-@app.route('/login')
-def login():
-    user = str(request.args.get('user'))
-    passw = str(request.args.get('passw'))
+    if user == []:
+        user = database.add({
+            'login': login,
+            'password': password,
+            'water': 80,
+            'oxygen': 40,
+            'energy': 0
+        })
+    else:
+        user = user[0]['id']
 
-    resp = make_response()
+    resp = make_response(redirect(url_for('application')))
 
-    resp.set_cookie(
-        'id',
-        str(database.getByQuery({
-            'user': user,
-            'passw': passw
-        })[0]['id'])
-    )
+    resp.set_cookie('id', str(user))
 
     return resp
 
-@app.route('/dashboard')
-def dashboard():
-    idx = request.cookies.get('id')
-    user = database.getById(int(idx))
-
-    return user['user']
-
 @app.route('/app')
-def app():
-    return render_template('base')
+def application():
+    user = database.getById(int(request.cookies.get('id')))
+    return render_template('dashboard.html', user=user)
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
